49a1d294bb35e7d1ef2889dc8731f56edc67c8cc
Emanuele Tritone <129194906+Malnutreeto@users.noreply.github.com>
2024-06-24 21:38:19 +0200

    Add files via upload

---
+++ invitation_post/style.js
@@ -0,0 +1,10 @@
+// const element = document.querySelectorAll('.flip-box-inner');
+
+
+//   element.addEventListener('touchstart', () => {
+//     element.classList.add('touch-hover-effect');
+//   });
+
+//   element.addEventListener('touchend', () => {
+//     element.classList.remove('touch-hover-effect');
+//   });
